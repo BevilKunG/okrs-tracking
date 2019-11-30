da659c7eb962a9f4e216554e3013c8a2af5d70f0
parinya <devilbevilgamer@gmail.com>
2019-11-30 20:49:15 +0700

    Updates

---
+++ precache-manifest.0430887dd15a850aee2f293f3ad626e5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e72e92b5877856e3c08821bbbe238fa0",
+    "url": "/okrs-tracking/index.html"
+  },
+  {
+    "revision": "e0048c36fda6898f74ee",
+    "url": "/okrs-tracking/static/js/2.8af4895c.chunk.js"
+  },
+  {
+    "revision": "4596083e06ccfc19300e",
+    "url": "/okrs-tracking/static/js/main.da88f2a8.chunk.js"
+  },
+  {
+    "revision": "b9a7fc102bbccba025db",
+    "url": "/okrs-tracking/static/js/runtime-main.eaf56b0d.js"
+  }
+]);
