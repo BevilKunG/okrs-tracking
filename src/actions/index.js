0c843a4b0cd8f77fa59ff8443b667c91ec443276
parinya <devilbevilgamer@gmail.com>
2019-12-01 22:46:57 +0700

    [ADDED] update objective (action creator)

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,20 @@
-import { ADD_OBJECTIVE } from './types'
+import { ADD_OBJECTIVE, UPDATE_OBJECTIVE } from './types'
 
 export const addObjective = (objective) => {
   return {
     type: ADD_OBJECTIVE,
-    objective
+    payload: {
+      objective
+    }
+  }
+}
+
+export const updateObjective = (updatedIndex, updatedObjective) => {
+  return {
+    type: UPDATE_OBJECTIVE,
+    payload: {
+      updatedIndex,
+      updatedObjective
+    }
   }
 }
--- src/reducers/ObjectiveReducer/ObjectivesReducer.js
+++ src/reducers/ObjectiveReducer/ObjectivesReducer.js
@@ -1,9 +1,16 @@
-import { ADD_OBJECTIVE } from '../../actions/types'
+import { ADD_OBJECTIVE, UPDATE_OBJECTIVE } from '../../actions/types'
 import { mockObjective } from './ObjectiveMock'
 
 export default (state = [mockObjective], action) => {
   switch(action.type) {
-    case ADD_OBJECTIVE: return [...state, action.objective]
+    case ADD_OBJECTIVE: return [...state, action.payload.objective]
+    case UPDATE_OBJECTIVE: {
+      return [
+      ...state.slice(0, action.payload.updatedIndex),
+      action.payload.updatedObjective,
+      ...state.slice(action.payload.updatedIndex + 1)
+      ]
+    }
+    default: return state
   }
-  return state
 }
