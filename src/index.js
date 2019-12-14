06f86e205359d4cd56caca856646cf71ebe6ca8a
parinya <devilbevilgamer@gmail.com>
2019-12-14 23:10:30 +0700

    [EDITED] test hash router for deploy gh-pages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import reducers from './reducers'
 import App from './App'
 
@@ -11,9 +11,9 @@ const store = createStore(reducers)
 const AppWithRedux = () => {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App/>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   )
 }
